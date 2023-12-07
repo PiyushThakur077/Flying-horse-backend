ae062dc06280365aefe55bd3d8bd52832c0487eb
PiyushThakur077 <thakurpiyush077@gmail.com>
2023-12-07 18:07:49 +0530

    new changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(StatusTableSeeder::class);
-        $this->call(UserTableSeeder::class);
+        // $this->call(StatusTableSeeder::class);
+        // $this->call(UserTableSeeder::class);
         $this->call(AdminSeeder::class);
     }
 }
