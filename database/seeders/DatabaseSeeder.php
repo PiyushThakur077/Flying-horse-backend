5b1486c1c32bdcd9f5bd02c8aaad2c945e4d0ed7
PiyushThakur077 <thakurpiyush077@gmail.com>
2023-12-07 18:45:48 +0530

    new changes icon

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(StatusTableSeeder::class);
-        // $this->call(UserTableSeeder::class);
+        $this->call(UserTableSeeder::class);
         $this->call(AdminSeeder::class);
     }
 }
