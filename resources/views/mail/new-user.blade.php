1817a8ea8e7343ba974586a59cb092d8a41bd1bd
PiyushThakur077 <thakurpiyush077@gmail.com>
2023-12-08 13:05:46 +0530

    email template changes

--- resources/views/mail/new-user.blade.php
+++ resources/views/mail/new-user.blade.php
@@ -18,14 +18,15 @@
                 <a href="{{url('/')}}">
                                 <img src="{{ asset('images/logo1.png') }}" class="logo" alt="{{config('app.name')}} Logo" style=" width: 50%;">
                             </a>
-               <h3 style="font-weight: 450;">Hello,   {{$name ?? ''}} </h3>
-                <h3 style="font-weight: 400;">
-                Welcome to flying horse! Your account has been successfullt created
-            </h3>
-            <p> Here are your login details: </p>
-            <p> Email: {{$email}}</p>
-            <p> Password: {{$password}}</p>
-            <p> Thank you for using our application</p>
+               <h3 style="font-weight: 300; font-size:25px;margin-bottom: 9px;">Greetings! <br><span style="font-size: 35px;font-weight: 500;color: #333333;">{{$name ?? ''}}</span></h3>
+                <h3 style="font-weight: 400;font-size: 21px;margin-top: 0;">
+                Welcome to flying horse! Your account has been successfully created
+            </h3><hr>
+            <p style="font-size: 20px;"> Here are your login details: </p>
+            <p style="font-size: 20px;"> Email: {{$email}}</p>
+            <p style="font-size: 20px;"> Password: {{$password}}</p>
+            <br>
+            <p style="font-size: 20px;"> For any query please contact your admin.</p>
             </td>
            
         </tr>
