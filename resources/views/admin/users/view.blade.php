4a2b8f2791b12b2c99ef0c2aa847db74df4c8b72
Yogesh Sharma <iyogesharma@gmail.com>
2024-02-26 22:04:11 +0530

    removed jquery issue

--- resources/views/admin/users/view.blade.php
+++ resources/views/admin/users/view.blade.php
@@ -61,7 +61,6 @@
 
 @push('scripts')
   
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.7.0.js" ></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js" ></script>
   
 </script>
